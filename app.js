cb7f81bb065d26ee45b14fe22fe5c0300a90a784
H.Y.C <chenghsienyu@aim.com>
2019-09-26 19:16:14 +0800

    add osc feature

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const osc = require("osc"),
+
 const Gpio = require('pigpio').Gpio;
 
 const led = new Gpio(18, { mode: Gpio.OUTPUT });
@@ -5,15 +7,66 @@ const led = new Gpio(18, { mode: Gpio.OUTPUT });
 let dutyCycle = 0;
 let counterIncrement = 1;
 
-setInterval(() => {
 
-    dutyCycle = dutyCycle + counterIncrement;
-    if (dutyCycle == 254) {
-        counterIncrement = -counterIncrement;
-    } else if (dutyCycle == 0) {
-        counterIncrement = counterIncrement * -1;
+/****************
+ * OSC Over UDP *
+ ****************/
+
+var getIPAddresses = function () {
+    var os = require("os"),
+        interfaces = os.networkInterfaces(),
+        ipAddresses = [];
+
+    for (var deviceName in interfaces) {
+        var addresses = interfaces[deviceName];
+        for (var i = 0; i < addresses.length; i++) {
+            var addressInfo = addresses[i];
+            if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+                ipAddresses.push(addressInfo.address);
+            }
+        }
     }
-    led.pwmWrite(dutyCycle);
-    console.log(dutyCycle);
 
-}, 50);
+    return ipAddresses;
+};
+
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 57121
+});
+
+udpPort.on("ready", function () {
+    var ipAddresses = getIPAddresses();
+
+    console.log("Listening for OSC over UDP.");
+    ipAddresses.forEach(function (address) {
+        console.log(" Host:", address + ", Port:", udpPort.options.localPort);
+    });
+});
+
+udpPort.on("message", function (oscMessage) {
+    if (oscMessage.address == '/play') {
+        setInterval(() => {
+
+            dutyCycle = dutyCycle + counterIncrement;
+            if (dutyCycle == 254) {
+                counterIncrement = -counterIncrement;
+            } else if (dutyCycle == 0) {
+                counterIncrement = counterIncrement * -1;
+            }
+            led.pwmWrite(dutyCycle);
+            console.log(dutyCycle);
+
+        }, 50);
+    }
+
+    if (oscMessage.address == '/stop' && omxplayer.running) {
+        led.pwmWrite(0);
+    }
+});
+
+udpPort.on("error", function (err) {
+    console.log(err);
+});
+
+udpPort.open();
